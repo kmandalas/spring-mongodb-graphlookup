adab99ea89a6a90d99d90ed84e13622d5474793d
k.mandalas <kyriakos@sweagle.expert>
2018-05-31 11:45:23 +0300

    added unit test

--- src/main/java/com/github/kmandalas/mongodb/application/Application.java
+++ src/main/java/com/github/kmandalas/mongodb/application/Application.java
@@ -2,8 +2,6 @@ package com.github.kmandalas.mongodb.application;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ArrayNode;
-import com.github.kmandalas.mongodb.documents.EntityType;
-import com.github.kmandalas.mongodb.documents.Node;
 import com.github.kmandalas.mongodb.repositories.NodeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -39,29 +37,29 @@ public class Application implements CommandLineRunner {
   @Override
   public void run(String... strings) throws Exception {
 
-    for (int c = 0; c < 20; c++) {
-      JsonNode myJson = Utils.readJson("static-tree.json");
-      List<Node> myNodeList = new ArrayList<>();
-
-      for (int i = 0; i < myJson.size(); i++) {
-        JsonNode cnode = myJson.get(i);
-        Node node = new Node();
-
-        node.setNodeId(cnode.get("id").asInt());
-        node.setName(cnode.get("name").asText());
-        node.setParentId(setParents((ArrayNode) cnode.get("parentId")));
-        node.setTenantId(cnode.get("tenantId").asInt());
-        node.setType(EntityType.Node);
-        node.setMds(cnode.get("isMds").asBoolean());
-        // node.setChangesetId(cnode.get("changesetId").asInt());
-        node.setChangesetId(c);
-
-        myNodeList.add(node);
-      }
-
-      nodeRepository.saveAll(myNodeList);
-    }
-
-    System.out.println("Import finished.");
+//    for (int c = 0; c < 20; c++) {
+//      JsonNode myJson = Utils.readJson("static-tree.json");
+//      List<Node> myNodeList = new ArrayList<>();
+//
+//      for (int i = 0; i < myJson.size(); i++) {
+//        JsonNode cnode = myJson.get(i);
+//        Node node = new Node();
+//
+//        node.setNodeId(cnode.get("id").asInt());
+//        node.setName(cnode.get("name").asText());
+//        node.setParentId(setParents((ArrayNode) cnode.get("parentId")));
+//        node.setTenantId(cnode.get("tenantId").asInt());
+//        node.setType(EntityType.Node);
+//        node.setMds(cnode.get("isMds").asBoolean());
+//        // node.setChangesetId(cnode.get("changesetId").asInt());
+//        node.setChangesetId(c);
+//
+//        myNodeList.add(node);
+//      }
+//
+//      nodeRepository.saveAll(myNodeList);
+//    }
+//
+//    System.out.println("Import finished.");
   }
 }
---
+++ src/test/java/com/github/kmandalas/mongodb/GraphLookupTests.java
@@ -0,0 +1,54 @@
+package com.github.kmandalas.mongodb;
+
+import com.github.kmandalas.mongodb.application.Application;
+import com.github.kmandalas.mongodb.repositories.NodeRepository;
+import org.bson.Document;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.aggregation.Aggregation;
+import org.springframework.data.mongodb.core.aggregation.GraphLookupOperation;
+import org.springframework.data.mongodb.core.aggregation.MatchOperation;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
+public class GraphLookupTests {
+
+  @Autowired
+  NodeRepository nodeRepository;
+
+  @Autowired
+  MongoTemplate mongoTemplate;
+
+  @Test
+  public void shouldRenderCorrectly() {
+
+    final Criteria isDimension = new Criteria("parentId").size(0);
+    final Criteria byChangesetId = new Criteria("changesetId").is(14);
+    final MatchOperation matchStage = Aggregation.match(isDimension.andOperator(byChangesetId));
+
+    GraphLookupOperation graphLookupOperation = GraphLookupOperation.builder() //
+            .from("treeView") //
+            .startWith("$nodeId") //
+            .connectFrom("nodeId") //
+            .connectTo("parentId") //
+            .maxDepth(0) //
+            .as("children");
+
+    Aggregation aggregation = Aggregation.newAggregation(matchStage, graphLookupOperation);
+
+    Document document = aggregation.toDocument("node", Aggregation.DEFAULT_CONTEXT);
+    System.out.println(document.toJson());
+
+    List<Document> results = mongoTemplate.aggregate(aggregation, "node", Document.class).getMappedResults();
+    assertThat(results.size() == 14);
+  }
+}
